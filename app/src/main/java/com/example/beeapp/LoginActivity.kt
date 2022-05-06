a6b2ae9d6ab95368117b04eb46ead7127458b26f
Alejandro Pizarroso Rodríguez <77840712+apizarroso1@users.noreply.github.com>
2022-05-06 19:19:54 +0200

    Initial commit + login & register activities

---
+++ app/src/main/java/com/example/beeapp/LoginActivity.kt
@@ -0,0 +1,65 @@
+package com.example.beeapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+
+class LoginActivity : AppCompatActivity() {
+
+    private lateinit var loginEmail: EditText
+    private lateinit var loginPassword: EditText
+    private lateinit var loginButton: Button
+    private lateinit var loginGoRegisterButton: Button
+    private lateinit var auth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        auth = FirebaseAuth.getInstance()
+
+        loginEmail = findViewById(R.id.loginEmail)
+        loginPassword = findViewById(R.id.loginPassword)
+        loginButton = findViewById(R.id.loginLoginButton)
+        loginGoRegisterButton = findViewById(R.id.loginGoRegisterButton)
+
+        loginButton.setOnClickListener {
+            val email = loginEmail.text.toString()
+            val password = loginPassword.text.toString()
+
+            if (checkEmpty(email, password)) {
+                login(email, password)
+            } else {
+                Toast.makeText(applicationContext, "There are empty fields", Toast.LENGTH_LONG)
+                    .show()
+            }
+        }
+
+        loginGoRegisterButton.setOnClickListener {
+            startActivity(Intent(this, RegisterActivity::class.java))
+            finish()
+        }
+
+    }
+
+    private fun login(email: String, password: String) {
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    startActivity(Intent(this, MainActivity::class.java))
+                    finish()
+                } else {
+                    Toast.makeText(applicationContext, "Login failed", Toast.LENGTH_LONG).show()
+                }
+            }
+    }
+
+    private fun checkEmpty(email: String, password: String): Boolean {
+        return email.isNotEmpty() && password.isNotEmpty()
+    }
+
+}
---
+++ app/src/main/java/com/example/beeapp/RegisterActivity.kt
@@ -0,0 +1,70 @@
+package com.example.beeapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+
+class RegisterActivity : AppCompatActivity() {
+
+    private lateinit var registerEmail: EditText
+    private lateinit var registerPassword: EditText
+    private lateinit var registerRepeatPassword: EditText
+    private lateinit var registerButton: Button
+    private lateinit var registerGoLoginButton: Button
+    private lateinit var auth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        auth = FirebaseAuth.getInstance()
+
+        registerEmail = findViewById(R.id.registerEmail)
+        registerPassword = findViewById(R.id.registerPassword)
+        registerRepeatPassword = findViewById(R.id.registerRepeatPassword)
+        registerButton = findViewById(R.id.registerRegisterButton)
+        registerGoLoginButton = findViewById(R.id.registerGoLoginButton)
+
+        registerButton.setOnClickListener {
+            val email = registerEmail.text.toString()
+            val password = registerPassword.text.toString()
+            val repeatPassword = registerRepeatPassword.text.toString()
+
+            if (password == repeatPassword && checkEmpty(email, password, repeatPassword)) {
+                register(email, password)
+            } else {
+                Toast.makeText(
+                    applicationContext,
+                    "Passwords don't match or there are empty fields",
+                    Toast.LENGTH_LONG
+                ).show()
+            }
+        }
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+
+    private fun register(email: String, password: String) {
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    Toast.makeText(
+                        applicationContext,
+                        "Account successfully created",
+                        Toast.LENGTH_LONG
+                    ).show()
+                    startActivity(Intent(this, MainActivity::class.java))
+                    finish()
+                } else {
+                    Toast.makeText(applicationContext, "Register failed", Toast.LENGTH_LONG).show()
+                }
+            }
+    }
+
+    private fun checkEmpty(email: String, password: String, repeatPassword: String): Boolean {
+        return email.isNotEmpty() && password.isNotEmpty() && repeatPassword.isNotEmpty()
+    }
+}
