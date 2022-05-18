8b9bc4fde2911a6bae4bf640dbd0d0f0c72b4a95
al3ks01 <vanualexp@gmail.com>
2022-05-18 13:23:07 +0200

    Backgraund de MainActivity

--- app/src/main/java/com/example/beeapp/MainActivity.kt
+++ app/src/main/java/com/example/beeapp/MainActivity.kt
@@ -1,11 +1,30 @@
 package com.example.beeapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+    lateinit var goUserButton: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initView()
+        initListeners()
+    }
+
+    private fun initView(){
+        goUserButton = findViewById(R.id.mainGoUserButton)
+    }
+
+    private fun initListeners(){
+        goUserButton.setOnClickListener { displayUser() }
+    }
+
+    private fun displayUser(){
+        //startActivity(Intent(this, UserActivity::class.java))
+        finish()
     }
 }
