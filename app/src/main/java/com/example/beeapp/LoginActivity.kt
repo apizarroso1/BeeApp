61e31848febf2726f2228d2b65d5f010d51c3993
al3ks01 <vanualexp@gmail.com>
2022-05-25 19:44:06 +0200

    Fix register layout

--- app/src/main/java/com/example/beeapp/LoginActivity.kt
+++ app/src/main/java/com/example/beeapp/LoginActivity.kt
@@ -8,7 +8,7 @@ import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatDelegate
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.FirebaseUser
+
 
 class LoginActivity : AppCompatActivity() {
 
