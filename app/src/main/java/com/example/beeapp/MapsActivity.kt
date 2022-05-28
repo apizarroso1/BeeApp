1b6893d04ec9e0f01280510f685071fe3cc2dc4e
Alejandro Pizarroso Rodríguez <77840712+apizarroso1@users.noreply.github.com>
2022-05-28 20:36:11 +0200

    Location services test

---
+++ app/src/main/java/com/example/beeapp/MapsActivity.kt
@@ -0,0 +1,49 @@
+package com.example.beeapp
+
+import android.content.pm.PackageManager
+import android.location.Location
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.tasks.Task
+
+class MapsActivity : AppCompatActivity() {
+
+    lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_maps)
+
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+
+        findViewById<Button>(R.id.btnGetLocation).setOnClickListener{
+            fetchLocation()
+        }
+    }
+
+    private fun fetchLocation() {
+        val task: Task<Location> = fusedLocationProviderClient.lastLocation
+
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                android.Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this,
+                android.Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION), 101)
+        }
+
+        task.addOnSuccessListener {
+            if (it != null){
+                Toast.makeText(this, "${it.latitude} ${it.longitude}", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+}
