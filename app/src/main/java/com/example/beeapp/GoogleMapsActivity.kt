bddbe6204924b1d77fca0b7ecb087c20eecf34c6
Alejandro Pizarroso Rodríguez <77840712+apizarroso1@users.noreply.github.com>
2022-05-29 14:08:52 +0200

    Google maps function added

---
+++ app/src/main/java/com/example/beeapp/GoogleMapsActivity.kt
@@ -0,0 +1,39 @@
+package com.example.beeapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import com.example.beeapp.databinding.ActivityGoogleMapsBinding
+
+class GoogleMapsActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private lateinit var mMap: GoogleMap
+    private lateinit var binding: ActivityGoogleMapsBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityGoogleMapsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+
+        // Add a marker in Sydney and move the camera 40.40681014415026, -3.5999011699561745
+        val villablanca = LatLng(40.40681014415026, -3.5999011699561745)
+        mMap.addMarker(MarkerOptions().position(villablanca).title("Marker in Villablanca Highschool"))
+        // Zoom 1world->20building
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(villablanca, 20f))
+    }
+}
