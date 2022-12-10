7e36f8a612bc6ecb6de7f34a98cedb9ebe1fbf01
al3ks01 <vanualexp@gmail.com>
2022-12-10 16:45:59 +0100

    pequeño cambio a expenses

--- app/src/main/java/com/example/beeapp/activity/ExpensesActivity.kt
+++ app/src/main/java/com/example/beeapp/activity/ExpensesActivity.kt
@@ -2,11 +2,41 @@ package com.example.beeapp.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import com.example.beeapp.R
+import com.example.beeapp.databinding.ActivityExpensesBinding
+import com.example.beeapp.databinding.ActivityUserBinding
 
 class ExpensesActivity : AppCompatActivity() {
+
+
+    private lateinit var viewBinding: ActivityExpensesBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_expenses)
+
+
+        viewBinding = ActivityExpensesBinding.inflate(layoutInflater)
+        setContentView(viewBinding.root)
+
+        supportActionBar?.title= "Expenses"
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+
+
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        when (item.itemId) {
+
+            android.R.id.home ->{
+                onBackPressed()
+                finish()
+                return true
+            }
+
+        }
+        return true
     }
 }
